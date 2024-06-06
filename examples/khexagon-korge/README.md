1b4747d36da48517da904a2c4490daa11514d554
offlinebrain <leonidivakhnenko@gmail.com>
2024-06-06 21:30:19 +0900

    init korge

---
+++ examples/khexagon-korge/README.md
@@ -0,0 +1,9 @@
+# Korge Hello World and Template
+
+This is a Hello World and Template for the KorGe game engine. Using gradle with kotlin-dsl.
+You can open this project in IntelliJ IDEA by opening the folder or the build.gradle.kts file.
+
+You can find this template at GitHub: <https://github.com/korlibs/korge-hello-world>
+
+
+You can find the KorGE documentation here: <https://docs.korge.org/korge/>
---
+++ examples/khexagon-korge/build.gradle.kts
@@ -0,0 +1,34 @@
+import korlibs.korge.gradle.*
+
+plugins {
+	alias(libs.plugins.korge)
+}
+
+korge {
+	id = "com.sample.demo"
+
+// To enable all targets at once
+
+	//targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+	
+	targetJvm()
+	targetJs()
+    targetWasm()
+	targetDesktop()
+	targetIos()
+	targetAndroid()
+
+	serializationJson()
+}
+
+
+dependencies {
+    add("commonMainApi", project(":deps"))
+    //add("commonMainApi", project(":korge-dragonbones"))
+}
+
